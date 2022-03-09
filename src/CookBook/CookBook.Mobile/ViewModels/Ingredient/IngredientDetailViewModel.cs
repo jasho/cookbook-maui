e22f34913fa7cd7528e5cc28693cd9b3755a2413
jasho <roman.jasek@hotmail.com>
2022-03-09 08:40:10 +0100

    Added contents of RecipeListView. Added missing resources to App.xaml. Udpated nuget packages.

--- src/CookBook/CookBook.Mobile/ViewModels/Ingredient/IngredientDetailViewModel.cs
+++ src/CookBook/CookBook.Mobile/ViewModels/Ingredient/IngredientDetailViewModel.cs
@@ -1,21 +1,29 @@
 ﻿using CookBook.Common.Models;
+using CookBook.Mobile.Api;
 
 namespace CookBook.Mobile.ViewModels.Ingredient;
 
 [QueryProperty(nameof(Id), "id")]
 public class IngredientDetailViewModel : ViewModelBase
 {
+    private readonly IIngredientsClient ingredientsClient;
+
     public string? Id { private get; set; }
 
     public IngredientDetailModel? Ingredient { get; set; }
 
+    public IngredientDetailViewModel(IIngredientsClient ingredientsClient)
+    {
+        this.ingredientsClient = ingredientsClient;
+    }
+
     public override async Task OnAppearingAsync()
     {
         await base.OnAppearingAsync();
-
+        
         if (Guid.TryParse(Id, out var id))
         {
-            Ingredient = new IngredientDetailModel(id, "Vejce", "Popis vejce", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Chicken_egg_2009-06-04.jpg/428px-Chicken_egg_2009-06-04.jpg");
+            Ingredient = await ingredientsClient.GetIngredientByIdAsync(id);
         }
     }
 }
--- src/CookBook/CookBook.Mobile/ViewModels/Recipe/RecipeListViewModel.cs
+++ src/CookBook/CookBook.Mobile/ViewModels/Recipe/RecipeListViewModel.cs
@@ -1,6 +1,45 @@
-﻿namespace CookBook.Mobile.ViewModels.Recipe
+﻿using CookBook.Common.Models;
+using CookBook.Mobile.Api;
+using CookBook.Mobile.Factories;
+using CookBook.Mobile.Services;
+using System.Windows.Input;
+
+namespace CookBook.Mobile.ViewModels.Recipe;
+
+public class RecipeListViewModel : ViewModelBase
 {
-    public class RecipeListViewModel : ViewModelBase
+    private readonly IRoutingService routingService;
+    private readonly IRecipesClient recipesClient;
+
+    public ICollection<RecipeListModel>? Items { get; set; }
+
+    public ICommand GoToDetailCommand { get; set; }
+    public ICommand GoToCreateCommand { get; set; }
+
+    public RecipeListViewModel(
+        IRoutingService routingService,
+        ICommandFactory commandFactory,
+        IRecipesClient recipesClient)
+    {
+        this.routingService = routingService;
+        this.recipesClient = recipesClient;
+
+        GoToDetailCommand = commandFactory.CreateCommand<Guid>(GoToDetailAsync);
+        GoToCreateCommand = commandFactory.CreateCommand<Guid>(GoToCreateAsync);
+    }
+
+    public override async Task OnAppearingAsync()
+    {
+        await base.OnAppearingAsync();
+
+        Items = await recipesClient.GetRecipesAllAsync();
+    }
+
+    private async Task GoToDetailAsync(Guid id)
+    {
+    }
+
+    private async Task GoToCreateAsync(Guid id)
     {
     }
 }
