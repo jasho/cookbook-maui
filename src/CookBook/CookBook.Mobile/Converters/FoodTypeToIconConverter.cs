b8bab946dd4cd8deda4a5376a84bcbe2fcba988d
jasho <roman.jasek@hotmail.com>
2022-03-15 21:20:30 +0100

    Added converters for label in RecipeList view

---
+++ src/CookBook/CookBook.Mobile/Converters/FoodTypeToIconConverter.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Maui.Converters;
+using CookBook.Common.Enums;
+using CookBook.Mobile.Resources.Fonts;
+
+namespace CookBook.Mobile.Converters;
+
+public class FoodTypeToIconConverter : BaseConverterOneWay<FoodType, string>
+{
+    public override string ConvertFrom(FoodType value)
+    {
+        var icon = FontAwesomeIcons.ConciergeBell;
+
+        switch (value)
+        {
+            case FoodType.MainDish:
+                icon = FontAwesomeIcons.ConciergeBell;
+                break;
+            case FoodType.Soup:
+                icon = FontAwesomeIcons.UtensilSpoon;
+                break;
+            case FoodType.Dessert:
+                icon = FontAwesomeIcons.IceCream;
+                break;
+        }
+
+        return icon;
+    }
+}
