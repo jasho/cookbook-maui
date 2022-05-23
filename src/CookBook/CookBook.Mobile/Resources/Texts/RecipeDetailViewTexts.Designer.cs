7a19d8300600e046846b94af594b69af244b9180
jasho <roman.jasek@hotmail.com>
2022-05-23 13:14:10 +0200

    Added texts for RecipeDetail view

--- src/CookBook/CookBook.Mobile/Resources/Texts/RecipeDetailViewTexts.Designer.cs
+++ src/CookBook/CookBook.Mobile/Resources/Texts/RecipeDetailViewTexts.Designer.cs
@@ -61,11 +61,29 @@ namespace CookBook.Mobile.Resources.Texts {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Back.
+        /// </summary>
+        public static string Back_Button_Text_Desktop {
+            get {
+                return ResourceManager.GetString("Back_Button_Text_Desktop", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Description.
         /// </summary>
         public static string Description_Label_Text {
             get {
                 return ResourceManager.GetString("Description_Label_Text", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Edit.
+        /// </summary>
+        public static string Edit_Button_Text_Desktop {
+            get {
+                return ResourceManager.GetString("Edit_Button_Text_Desktop", resourceCulture);
             }
         }
         
