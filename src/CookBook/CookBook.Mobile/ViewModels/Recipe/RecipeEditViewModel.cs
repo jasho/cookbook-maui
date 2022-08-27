17b74fc7e93ba3fe6c26feee92ef5edf38050254
jasho <roman.jasek@hotmail.com>
2022-08-27 18:43:01 +0200

    Added API calls to RecipeEdit view for saving and deleting a recipe

--- src/CookBook/CookBook.Mobile/ViewModels/Recipe/RecipeEditViewModel.cs
+++ src/CookBook/CookBook.Mobile/ViewModels/Recipe/RecipeEditViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CookBook.Common.Enums;
 using CookBook.Common.Models;
 using CookBook.Mobile.Api;
+using Microsoft.Toolkit.Mvvm.Input;
 
 namespace CookBook.Mobile.ViewModels;
 
@@ -18,5 +19,19 @@ public partial class RecipeEditViewModel : ViewModelBase
         this.recipesClient = recipesClient;
 
         FoodTypes = new List<FoodType>((FoodType[])Enum.GetValues(typeof(FoodType)));
+    }
+
+    [ICommand]
+    private async Task DeleteAsync()
+    {
+        await recipesClient.DeleteRecipeAsync(Recipe.Id.Value);
+        await Shell.Current.GoToAsync("../");
+    }
+
+    [ICommand]
+    private async Task SaveAsync()
+    {
+        await recipesClient.UpdateRecipeAsync(Recipe);
+        await Shell.Current.GoToAsync("../");
     }
 }
