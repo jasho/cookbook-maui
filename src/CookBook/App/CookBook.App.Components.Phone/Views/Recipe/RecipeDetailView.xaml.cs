35fd09e5c8f7e10de3e4a2507085f0e46f0b346b
jasho <roman.jasek@hotmail.com>
2023-03-20 12:37:28 +0100

    Added RecipeEdit view to Component projects. Fixed logical name for ingredient_placeholder image

---
+++ src/CookBook/App/CookBook.App.Components.Phone/Views/Recipe/RecipeDetailView.xaml.cs
@@ -0,0 +1,12 @@
+using CookBook.App.Components.Common.ViewModels;
+
+namespace CookBook.App.Components.Views;
+
+public partial class RecipeDetailView
+{
+	public RecipeDetailView(RecipeDetailViewModel viewModel)
+		: base(viewModel)
+    {
+		InitializeComponent();
+	}
+}
