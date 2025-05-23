03235fc76e617d7e5c72de0984b95c7e940f8021
jasho <roman.jasek@hotmail.com>
2025-05-23 14:46:35 +0200

    Add individual extension classes for different data types for IdiomExtension. Fix application launch.

---
+++ src/CookBook/CookBook.Maui/CookBook.Maui.Common/Extensions/IdiomExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace CookBook.Maui.Extensions;
+
+[AcceptEmptyServiceProvider]
+public class IdiomExtension<T> : IMarkupExtension
+{
+    public T? Phone { get; set; }
+    public T? Desktop { get; set; }
+    public virtual object? ProvideValue(IServiceProvider serviceProvider)
+    {
+#if DESKTOP
+        return Desktop;
+#elif PHONE
+        return Phone;
+#endif
+    }
+}
+
+public class IdiomIntExtension : IdiomExtension<int>;
+public class IdiomDoubleExtension : IdiomExtension<double>;
+public class IdiomThicknessExtension : IdiomExtension<Thickness>;
+public class IdiomBoolExtension : IdiomExtension<bool>;
+public class IdiomObjectExtension : IdiomExtension<object>;
