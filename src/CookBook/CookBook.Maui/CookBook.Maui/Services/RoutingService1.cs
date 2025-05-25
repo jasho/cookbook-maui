6d83eac9d51416b70250c26268c3ab3a89af52e9
Roman Jašek <roman.jasek@hotmail.com>
2025-05-25 16:18:07 +0200

    Remove custom release configurations

--- src/CookBook/CookBook.Maui/CookBook.Maui/Services/RoutingService1.cs
+++ src/CookBook/CookBook.Maui/CookBook.Maui/Services/RoutingService1.cs
@@ -1,41 +1,41 @@
-﻿//using CookBook.Maui.Models;
-//using CookBook.Maui.Pages;
+﻿using CookBook.Maui.Models;
+using CookBook.Maui.Pages;
 
-//namespace CookBook.Maui.Services;
+namespace CookBook.Maui.Services;
 
-//public class RoutingService1 : RoutingService
-//{
-//    private static readonly IEnumerable<RouteModel> routesPhone =
-//        RoutesCommon.Concat(
-//            new List<RouteModel>
-//        {
-//            new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientDetailRouteRelative, typeof(IngredientDetailPagePhone)),
+public class RoutingService1 : RoutingService
+{
+    private static readonly IEnumerable<RouteModel> routesPhone =
+        RoutesCommon.Concat(
+            new List<RouteModel>
+        {
+            new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientDetailRouteRelative, typeof(IngredientDetailPagePhone)),
 
-//            new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientEditRouteRelative, typeof(IngredientEditPagePhone)),
-//            new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientDetailRouteRelative + RoutingConstants.IngredientEditRouteRelative , typeof(IngredientEditPagePhone)),
+            new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientEditRouteRelative, typeof(IngredientEditPagePhone)),
+            new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientDetailRouteRelative + RoutingConstants.IngredientEditRouteRelative , typeof(IngredientEditPagePhone)),
 
-//            new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeDetailRouteRelative, typeof(RecipeDetailPagePhone)),
+            new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeDetailRouteRelative, typeof(RecipeDetailPagePhone)),
 
-//        });
+        });
 
-//    private static readonly IEnumerable<RouteModel> routesDesktop =
-//        RoutesCommon.Concat(
-//            new List<RouteModel>
-//            {
-//                new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientDetailRouteRelative, typeof(IngredientDetailPageDesktop)),
-//                new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientEditRouteRelative, typeof(IngredientEditPageDesktop)),
-//                new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientDetailRouteRelative + RoutingConstants.IngredientEditRouteRelative, typeof(IngredientEditPageDesktop)),
+    private static readonly IEnumerable<RouteModel> routesDesktop =
+        RoutesCommon.Concat(
+            new List<RouteModel>
+            {
+                new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientDetailRouteRelative, typeof(IngredientDetailPageDesktop)),
+                new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientEditRouteRelative, typeof(IngredientEditPageDesktop)),
+                new(RoutingConstants.IngredientListRouteAbsolute + RoutingConstants.IngredientDetailRouteRelative + RoutingConstants.IngredientEditRouteRelative, typeof(IngredientEditPageDesktop)),
 
-//                new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeDetailRouteRelative, typeof(RecipeDetailPageDesktop)),
-//                new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeEditRouteRelative, typeof(RecipeEditPageDesktop)),
+                new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeDetailRouteRelative, typeof(RecipeDetailPageDesktop)),
+                new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeEditRouteRelative, typeof(RecipeEditPageDesktop)),
 
-//                new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeDetailRouteRelative + RoutingConstants.RecipeEditRouteRelative, typeof(RecipeIngredientsEditPageDesktop)),
-//                new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeEditRouteRelative + RoutingConstants.RecipeIngredientsEditRouteRelative, typeof(RecipeIngredientsEditPageDesktop)),
-//                new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeDetailRouteRelative + RoutingConstants.RecipeEditRouteRelative + RoutingConstants.RecipeIngredientsEditRouteRelative, typeof(RecipeIngredientsEditPageDesktop)),
-//            });
+                new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeDetailRouteRelative + RoutingConstants.RecipeEditRouteRelative, typeof(RecipeIngredientsEditPageDesktop)),
+                new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeEditRouteRelative + RoutingConstants.RecipeIngredientsEditRouteRelative, typeof(RecipeIngredientsEditPageDesktop)),
+                new(RoutingConstants.RecipeListRouteAbsolute + RoutingConstants.RecipeDetailRouteRelative + RoutingConstants.RecipeEditRouteRelative + RoutingConstants.RecipeIngredientsEditRouteRelative, typeof(RecipeIngredientsEditPageDesktop)),
+            });
 
-//    public override IEnumerable<RouteModel> Routes
-//        => DeviceInfo.Idiom == DeviceIdiom.Phone
-//            ? routesPhone
-//            : routesDesktop;
-//}
+    public override IEnumerable<RouteModel> Routes
+        => DeviceInfo.Idiom == DeviceIdiom.Phone
+            ? routesPhone
+            : routesDesktop;
+}
